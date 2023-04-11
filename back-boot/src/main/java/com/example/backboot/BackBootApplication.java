88fd42b1af1a4c35b2d9851348c2221adfc84332
wang-zhihongggg <2417535698@qq.com>
2023-04-11 18:44:58 +0800

    Merge remote-tracking branch 'origin/master'

---
+++ back-boot/src/main/java/com/example/backboot/BackBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.backboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackBootApplication.class, args);
+    }
+
+}
---
+++ back-boot/src/test/java/com/example/backboot/BackBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
